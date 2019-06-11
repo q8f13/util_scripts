dcf3861e6682d6128cb00d400795c4d8f47d898b
q8f13 <dustfall@gmail.com>
2019-06-11 17:46:52 +0800

    automatic add folder to save slices

--- slicer.py
+++ slicer.py
@@ -13,5 +13,9 @@ import os, sys
 # print('slice %s pieces' % sys.argv[2])
 image_slicer.Image.MAX_IMAGE_PIXELS = 9999999999
 tiles = image_slicer.slice(sys.argv[1], int(sys.argv[2]), save=False)
+folder = './sliced'
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
 image_slicer.save_tiles(tiles, prefix='slice', format='jpeg', directory='./sliced/')
 # image_slicer.save_tiles(tiles, prefix='slice', format='jpeg', directory='./sliced/')
